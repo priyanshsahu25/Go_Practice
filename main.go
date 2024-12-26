af10f56d8b5fd110a8d2865d2b3a5877bede9ce3
Priyansh Sahu <priyansh.sahu25@gmail.com>
2024-12-26 13:32:56 +0000

    commit

--- main.go
+++ main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
+func show(arr []any) {
+	fmt.Println(arr)
+}
+
+type Person struct {
+	name   string
+	gender any
+	call   any
+}
+
+func sum[T any](arr []T) {
+	for _, val := range arr {
+		fmt.Println(val)
+	}
+}
+
+func infr(arr []interface{}) {
+	fmt.Println(arr)
+}
+
 func main() {
-	fmt.Println("Hello, WOrld I am back")
+	arrstr := []string{"huhuh", "jiii"}
+	arrint := []int{1, 2}
+	sum(arrstr)
+	sum(arrint)
+	newarr := []interface{}{"hello", 1}
+	fmt.Println(newarr)
+
+	mpp := make(map[any]any)
+	mpp["hello"] = 1
+	mpp[22] = 11
+
+
+	Priyansh := Person{"Sahu", 1, "jhjh"}
+	Jivesh := Person{"jjj", "jbjbn", "njnjn"}
+	fmt.Println(Priyansh, Jivesh)
+
+	mpp1 := make(map[any]any)
+	mpp1["ID"] = 1
+	mpp1[123] = "Hello"
+	fmt.Println(mpp1)
 }
